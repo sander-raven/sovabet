692e883c386dfcf65cb657dc1bf90c0f885a31a0
Alexander Aravin <sander-raven@yandex.ru>
2022-07-08 12:27:41 +0300

    Add verbose_name to predictions app

--- src/predictions/apps.py
+++ src/predictions/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PredictionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'predictions'
+    verbose_name = 'Прогнозы'
