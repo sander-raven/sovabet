d7fdb9e67479b992307826991ed45393f9f92bf8
Alexander Aravin <sander-raven@yandex.ru>
2022-07-11 21:06:56 +0300

    Add Season, Tournament and Game models

--- src/predictions/admin.py
+++ src/predictions/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from .models import (
-    Event, PredictedEvent, PredictedRound, Predictor, Round, Season, Tournament
+    Season,
+    Tournament,
+    Game,
 )
 
 
@@ -10,111 +12,33 @@ admin.site.site_title = "SOVABET"
 admin.site.index_title = "Администрирование SOVABET"
 
 
-@admin.register(Season)
-class SeasonAdmin(admin.ModelAdmin):
+class DefaultAdmin(admin.ModelAdmin):
     list_display = ("__str__", "is_active")
-    search_fields = ("title", "info")
-    fields = ("title", "info", "is_active", "created_at", "modified_at")
+    search_fields = ("name", "info")
+    fields = ("name", "info", "is_active", "created_at", "modified_at")
     readonly_fields = ("created_at", "modified_at")
+
+    class Meta:
+        abstract = True
+
+
+@admin.register(Season)
+class SeasonAdmin(DefaultAdmin):
+    pass
 
 
 @admin.register(Tournament)
-class TournamentAdmin(admin.ModelAdmin):
+class TournamentAdmin(DefaultAdmin):
     list_display = ("__str__", "season", "is_active")
-    search_fields = ("title", "info", "season__title", "season__info")
+    search_fields = ("name", "info", "season__name", "season__info")
     fields = (
-        "title", "info", "is_active", "season", "created_at", "modified_at"
+        "name", "info", "is_active", "season", "created_at", "modified_at"
     )
-    readonly_fields = ("created_at", "modified_at")
 
 
-class EventInline(admin.TabularInline):
-    model = Event
-    extra = 3
-    max_num = 3
-
-
-@admin.register(Round)
-class RoundAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "is_active")
-    search_fields = ("title", "info", "tournament__title", "tournament__info")
+@admin.register(Game)
+class RoundAdmin(DefaultAdmin):
+    search_fields = ("name", "info", "tournament__name", "tournament__name")
     fields = (
-        "title", "info", "is_active", "tournament", "created_at", "modified_at"
+        "name", "info", "is_active", "tournament", "created_at", "modified_at"
     )
-    readonly_fields = ("created_at", "modified_at")
-    inlines = (EventInline, )
-
-
-@admin.register(Event)
-class EventAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "round")
-    search_fields = ("description", "result")
-    fields = ("description", "result", "round", "created_at", "modified_at")
-    readonly_fields = ("created_at", "modified_at")
-
-
-@admin.register(Predictor)
-class PredictorAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "id", "vk_id")
-    search_fields = ("name", )
-    fields = ("name", "vk_id", "created_at", "modified_at")
-    readonly_fields = ("created_at", "modified_at")
-
-
-class PredictedEventInline(admin.TabularInline):
-    model = PredictedEvent
-    extra = 3
-    max_num = 3
-    readonly_fields = ("points", )
-
-    def get_formset(self, request, obj=None, **kwargs):
-        self.parent_obj = obj
-        return super(PredictedEventInline, self).get_formset(request, obj, **kwargs)
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "event":
-            if self.parent_obj and self.parent_obj.round:
-                kwargs["queryset"] = Event.objects.filter(
-                    round=self.parent_obj.round
-                )
-            else:
-                kwargs["queryset"] = Event.objects.none()
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-
-@admin.register(PredictedRound)
-class PredictedRoundAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "total_points")
-    fields = (
-        "predictor", "round", "total_points", "created_at", "modified_at"
-    )
-    readonly_fields = ("total_points", "created_at", "modified_at")
-    inlines = (PredictedEventInline, )
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "round":
-            kwargs["queryset"] = Round.objects.filter(is_active=True)
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-
-@admin.register(PredictedEvent)
-class PredictedEventAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "points", "predicted_round", "event")
-    search_fields = ("description", "result", "predicted_round", "event")
-    fields = (
-        "description", "result", "predicted_round", "event",
-        "points", "created_at", "modified_at"
-    )
-    readonly_fields = ("points", "created_at", "modified_at")
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "event":
-            object_id = request.resolver_match.kwargs.get('object_id')
-            predicted_event = self.get_object(request, object_id)
-            if predicted_event and predicted_event.predicted_round:
-                kwargs["queryset"] = Event.objects.filter(
-                    round=predicted_event.predicted_round.round
-                )
-            else:
-                kwargs["queryset"] = Event.objects.none()
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
--- src/predictions/models.py
+++ src/predictions/models.py
@@ -1,27 +1,34 @@
 from django.db import models
 
 
-class Season(models.Model):
-    title = models.CharField("заголовок", max_length=50, unique=True)
+class Timestamp(models.Model):
+    created_at = models.DateTimeField("создание", auto_now_add=True)
+    modified_at = models.DateTimeField("изменение", auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class CommonInfo(Timestamp):
+    name = models.CharField("название", max_length=50, unique=True)
     info = models.TextField("информация", blank=True)
-    is_active = models.BooleanField("действующий?", default=True)
-    created_at = models.DateTimeField("создан", auto_now_add=True)
-    modified_at = models.DateTimeField("изменён", auto_now=True)
+    is_active = models.BooleanField("актив?", default=True)
+
+    class Meta:
+        abstract = True
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Season(CommonInfo):
 
     class Meta:
         verbose_name = "сезон"
         verbose_name_plural = "сезоны"
-    
-    def __str__(self) -> str:
-        return self.title
 
 
-class Tournament(models.Model):
-    title = models.CharField("заголовок", max_length=50, unique=True)
-    info = models.TextField("информация", blank=True)
-    is_active = models.BooleanField("действующий?", default=True)
-    created_at = models.DateTimeField("создан", auto_now_add=True)
-    modified_at = models.DateTimeField("изменён", auto_now=True)
+class Tournament(CommonInfo):
     season = models.ForeignKey(
         Season,
         on_delete=models.PROTECT,
@@ -32,136 +39,19 @@ class Tournament(models.Model):
     class Meta:
         verbose_name = "турнир"
         verbose_name_plural = "турниры"
-    
-    def __str__(self) -> str:
-        return self.title
 
 
-class Round(models.Model):
-    title = models.CharField("заголовок", max_length=50)
-    info = models.TextField("информация", blank=True)
-    is_active = models.BooleanField("действующий?", default=True)
-    created_at = models.DateTimeField("создан", auto_now_add=True)
-    modified_at = models.DateTimeField("изменён", auto_now=True)
+class Game(CommonInfo):
     tournament = models.ForeignKey(
         Tournament,
         on_delete=models.PROTECT,
-        related_name="rounds",
+        related_name="games",
         verbose_name="турнир",
     )
 
     class Meta:
-        verbose_name = "раунд"
-        verbose_name_plural = "раунды"
+        verbose_name = "игра"
+        verbose_name_plural = "игры"
     
     def __str__(self) -> str:
-        return f"{self.title} :: {self.tournament}"
-
-    def calculate_points(self):
-        events = Event.objects.filter(round=self)
-        predicted_rounds = PredictedRound.objects.filter(round=self)
-        for prediction in predicted_rounds:
-            predicted_events = PredictedEvent.objects.filter(
-                predicted_round=prediction
-            )
-            for predicted_event in predicted_events:
-                current_event_points = 0
-                if predicted_event.result in events.values_list("result", flat=True):
-                    current_event_points += 2
-                    if predicted_event.result == predicted_event.event.result:
-                        current_event_points += 1
-                        if predicted_event.event.description == "1":
-                            current_event_points += 1
-                predicted_event.points = current_event_points
-                predicted_event.save()
-            total_points = predicted_events.aggregate(models.Sum("points"))
-            prediction.total_points = total_points["points__sum"]
-            prediction.save()
-
-
-class Event(models.Model):
-    description = models.CharField("описание", max_length=100)
-    result = models.CharField("результат", max_length=100, blank=True)
-    created_at = models.DateTimeField("создано", auto_now_add=True)
-    modified_at = models.DateTimeField("изменено", auto_now=True)
-    round = models.ForeignKey(
-        Round,
-        on_delete=models.PROTECT,
-        related_name="events",
-        verbose_name="раунд",
-    )
-
-    class Meta:
-        verbose_name = "событие"
-        verbose_name_plural = "события"
-    
-    def __str__(self) -> str:
-        return f"{self.description} >> {self.result}"
-
-
-class Predictor(models.Model):
-    name = models.CharField("имя", max_length=50)
-    vk_id = models.CharField("VK ID", max_length=100, blank=True)
-    created_at = models.DateTimeField("создан", auto_now_add=True)
-    modified_at = models.DateTimeField("изменён", auto_now=True)
-
-    class Meta:
-        verbose_name = "прогнозист"
-        verbose_name_plural = "прогнозисты"
-    
-    def __str__(self) -> str:
-        return self.name
-
-
-class PredictedRound(models.Model):
-    predictor = models.ForeignKey(
-        Predictor,
-        on_delete=models.CASCADE,
-        related_name="predicted_rounds",
-        verbose_name="прогнозист",
-    )
-    round = models.ForeignKey(
-        Round,
-        on_delete=models.CASCADE,
-        related_name="predicted_rounds",
-        verbose_name="раунд",
-    )
-    total_points = models.FloatField("сумма баллов", default=0.0)
-    created_at = models.DateTimeField("создан", auto_now_add=True)
-    modified_at = models.DateTimeField("изменён", auto_now=True)
-
-    class Meta:
-        verbose_name = "прогнозируемый раунд"
-        verbose_name_plural = "прогнозируемые раунды"
-    
-    def __str__(self) -> str:
-        return f"Прогноз {self.predictor} для {self.round}"
-
-
-class PredictedEvent(models.Model):
-    description = models.CharField("описание", max_length=100)
-    result = models.CharField("результат", max_length=100, blank=True)
-    created_at = models.DateTimeField("создано", auto_now_add=True)
-    modified_at = models.DateTimeField("изменено", auto_now=True)
-    points = models.FloatField("баллы", default=0.0)
-    predicted_round = models.ForeignKey(
-        PredictedRound,
-        on_delete=models.CASCADE,
-        related_name="predicted_events",
-        verbose_name="прогнозируемый раунд",
-    )
-    event = models.ForeignKey(
-        Event,
-        on_delete=models.CASCADE,
-        related_name="predicted_events",
-        verbose_name="событие",
-        null=True,
-        blank=True,
-    )
-
-    class Meta:
-        verbose_name = "прогнозируемое событие"
-        verbose_name_plural = "прогнозируемые события"
-    
-    def __str__(self) -> str:
-        return f"{self.description} >> {self.result}"
+        return f"{self.name} :: {self.tournament}"
