d88e970081b406763686f7ffacc0cdd560e7b278
Alexander Aravin <sander-raven@yandex.ru>
2022-07-15 18:37:14 +0300

    Add some functionality
    Add RankedPerformances.
    Add get_ranked_performances.
    Add calculate_game_results.
    Rename PointsForPredictionEvents to Points.

--- src/predictions/logic.py
+++ src/predictions/logic.py
@@ -1,6 +1,7 @@
 """Application Business Logic."""
 
 
+from collections import namedtuple
 from enum import Enum
 
 from django.db.models.aggregates import Sum
@@ -14,12 +15,22 @@ from predictions.models import (
 )
 
 
-class PointsForPredictionEvent(Enum):
+class Points(Enum):
     WINNER_MATCHED = 4
     RUNNER_UP_MATCHED = 3
     THIRD_PLACE_MATCHED = 3
     TEAM_WAS_AWARDED = 2
     NO_MATCHES = 0
+
+
+RankedPerformances = namedtuple(
+    "RankedPerformances",
+    [
+        "winner",
+        "runner_up",
+        "third_place",
+    ]
+)
 
 
 def get_not_null_performances_for_game(game: Game) -> QuerySet[Performance]:
@@ -30,6 +41,42 @@ def get_not_null_performances_for_game(game: Game) -> QuerySet[Performance]:
         game=game, result__isnull=False
     ).order_by("result")
     return performances
+
+
+def get_ranked_performances(
+    performances: QuerySet[Performance]
+) -> RankedPerformances:
+
+    prep_performances = []
+
+    # winner
+    performance = performances.filter(result__id=1).first()
+    prep_performances.append(
+        {
+            "performance": performance,
+            "points": Points.WINNER_MATCHED.value,
+        }
+    )
+
+    # runner up
+    performance = performances.filter(result__id=2).first()
+    prep_performances.append(
+        {
+            "performance": performance,
+            "points": Points.RUNNER_UP_MATCHED.value,
+        }
+    )
+
+    # third place
+    performance = performances.filter(result__id=3).first()
+    prep_performances.append(
+        {
+            "performance": performance,
+            "points": Points.THIRD_PLACE_MATCHED.value,
+        }
+    )
+
+    return RankedPerformances(*prep_performances)
 
 
 def get_game_predictions(game: Game) -> QuerySet[Prediction]:
@@ -58,3 +105,40 @@ def calculate_total_points_for_prediction(
     total_points = prediction_events.aggregate(Sum("points"))
     prediction.total_points = total_points["points__sum"]
     prediction.save()
+
+
+def calculate_game_results(game: Game) -> None:
+    performances = get_not_null_performances_for_game(game)
+    ranked_performances = get_ranked_performances(performances)
+    predictions = get_game_predictions(game)
+
+    for prediction in predictions:
+        prediction_events = get_prediction_events(prediction)
+        events = [
+            event for event in prediction_events
+        ]
+        for event in events:
+            event.points = Points.NO_MATCHES.value
+
+        for ranked_performance in ranked_performances:
+            performance = ranked_performance.get("performance")
+            if performance:
+                full_hit = [
+                    event for event in events
+                    if event.result==performance.result and
+                    event.team==performance.team
+                ]
+                if full_hit:
+                    full_hit[0].points = ranked_performance.get("points")
+                else:
+                    prizes_hit = [
+                        event for event in events
+                        if event.team==performance.team
+                    ]
+                    if prizes_hit:
+                        prizes_hit[0].points = Points.TEAM_WAS_AWARDED.value
+
+        for event in events:
+            event.save()
+
+        calculate_total_points_for_prediction(prediction, prediction_events)
