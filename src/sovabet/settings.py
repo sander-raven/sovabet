45010cd77fe3fffcc665a5cfb85e1476a875e92e
Alexander Aravin <sander-raven@yandex.ru>
2022-07-18 23:16:20 +0300

    Add django-import-export

--- src/sovabet/settings.py
+++ src/sovabet/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'predictions',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# django-import-export
+IMPORT_EXPORT_USE_TRANSACTIONS = True
